a11a8202c77cf092003abf9ad433b6a985c8641a
Olena-L-Bondarchuk <114254878+Olena-L-Bondarchuk@users.noreply.github.com>
2023-03-24 13:01:08 +0000

    Deploying to gh-pages from @ Olena-L-Bondarchuk/goit-js-hw-09@51e9aca465c86ee25a9b1b1f66270e643d8093d7 🚀

---
+++ 01-color-switcher.f8833ace.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(d){t.disabled=!0,e.disabled=!1,n=setInterval(a,1e3)})),e.addEventListener("click",(function(a){t.disabled=!1,e.disabled=!0,clearInterval(n)}));var n=null;function a(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}e.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.f8833ace.js.map
